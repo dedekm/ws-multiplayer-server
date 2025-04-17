f9fb35db85550ec197e7d60600794db2f5771bae
Martin Dedek <dedekmm@gmail.com>
2025-04-17 12:33:02 +0200

    send user input with interval

--- public/script.js
+++ public/script.js
@@ -7,9 +7,33 @@ function sendInput(x, y) {
   ws.send(JSON.stringify({ x: x, y: y }));
 }
 
+const keysPressed = {
+  ArrowLeft: false,
+  ArrowRight: false,
+  ArrowUp: false,
+  ArrowDown: false,
+};
+
 document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowLeft") sendInput(1, 0);
-  if (e.key === "ArrowRight") sendInput(-1, 0);
-  if (e.key === "ArrowUp") sendInput(0, 1);
-  if (e.key === "ArrowDown") sendInput(0, -1);
+  if (e.key in keysPressed) {
+    keysPressed[e.key] = true;
+  }
 });
+
+document.addEventListener("keyup", (e) => {
+  if (e.key in keysPressed) {
+    keysPressed[e.key] = false;
+  }
+});
+
+setInterval(() => {
+  let x = 0;
+  let y = 0;
+
+  if (keysPressed["ArrowLeft"]) x += 1;
+  if (keysPressed["ArrowRight"]) x -= 1;
+  if (keysPressed["ArrowUp"]) y += 1;
+  if (keysPressed["ArrowDown"]) y -= 1;
+
+  if (x !== 0 || y !== 0) sendInput(x, y);
+}, 1000 / 30);
