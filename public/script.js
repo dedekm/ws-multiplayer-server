217e3b3118dfcd8c45cac9b61b0da9685023eb11
Martin Dedek <dedekmm@gmail.com>
2025-04-27 20:37:49 +0200

    update player's input, add damage effect [wip]

--- public/script.js
+++ public/script.js
@@ -3,29 +3,32 @@ let ws = new WebSocket("ws://" + location.host + "/");
 ws.onopen = () => console.log("connected to server");
 ws.onmessage = (event) => {
   const data = JSON.parse(event.data);
-  const element = document.querySelector('.logs');
+  const element = document.querySelector(".logs");
 
-  switch(data.action) {
-    case 'damaged':
+  switch (data.action) {
+    case "damaged":
       element.innerHTML += `<p>damaged, current health: ${data.health}</p>`;
-      const title = document.querySelector('.title');
+      const title = document.querySelector(".title");
 
-      title.style.backgroundColor = 'red';
-      title.style.transition = 'background-color 0.1s linear';
+      title.style.backgroundColor = "red";
+      title.style.transition = "background-color 0.1s linear";
 
       setTimeout(() => {
-        title.style.transition = 'background-color 1s ease-out';
-        title.style.backgroundColor = '';
+        title.style.transition = "background-color 1s ease-out";
+        title.style.backgroundColor = "";
       }, 600);
 
       break;
     default:
       console.log(`Unknown action: ${data.action}`);
   }
-}
-function sendInput(x, y, shoot) {
-  ws.send(JSON.stringify({ x: x, y: y, shoot: shoot }));
-}
+};
+
+let currentInput = {
+  x: 0,
+  y: 0,
+  shoot: false,
+};
 
 const keysPressed = {
   ArrowLeft: false,
@@ -34,6 +37,20 @@ const keysPressed = {
   ArrowDown: false,
   Space: false,
 };
+
+function sendInput(input) {
+  const inputDiff = {};
+
+  Object.keys(input).forEach((key) => {
+    if (currentInput[key] !== input[key]) inputDiff[key] = input[key];
+  });
+
+  if (Object.keys(inputDiff).length !== 0) {
+    ws.send(JSON.stringify(inputDiff));
+  }
+
+  currentInput = input;
+}
 
 document.addEventListener("keydown", (e) => {
   if (e.code in keysPressed) {
@@ -48,15 +65,17 @@ document.addEventListener("keyup", (e) => {
 });
 
 setInterval(() => {
-  let x = 0;
-  let y = 0;
-  let shoot = false;
+  const input = {
+    x: 0,
+    y: 0,
+    shoot: false,
+  };
 
-  if (keysPressed["ArrowLeft"]) x += 1;
-  if (keysPressed["ArrowRight"]) x -= 1;
-  if (keysPressed["ArrowUp"]) y += 1;
-  if (keysPressed["ArrowDown"]) y -= 1;
-  if (keysPressed["Space"]) shoot = true;
+  if (keysPressed["ArrowLeft"]) input.x -= 1;
+  if (keysPressed["ArrowRight"]) input.x += 1;
+  if (keysPressed["ArrowUp"]) input.y += 1;
+  if (keysPressed["ArrowDown"]) input.y -= 1;
+  if (keysPressed["Space"]) input.shoot = true;
 
-  if (x !== 0 || y !== 0 || shoot) sendInput(x, y, shoot);
+  sendInput(input);
 }, 1000 / 30);
